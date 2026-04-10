aa53d2aa6a8f3a5da685b1a4bc3caaa87b2fd131
일혁 <dnfjddk2@gmail.com>
2026-04-10 22:44:39 +0900

    Implement unified `Controller` interface and refactor controllers to use `ModelAndView`

--- src/main/java/controller/MemberDetailController.java
+++ src/main/java/controller/MemberDetailController.java
@@ -1,4 +1,17 @@
 package controller;
 
-public class MemberDetailController {
+import frontcontroller.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.MemberService;
+
+public class MemberDetailController implements Controller {
+
+    private final MemberService service = MemberService.getInstance();
+
+    @Override
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        Long id = Long.parseLong(request.getParameter("id"));
+        return new ModelAndView("detail").addObject("member", service.findById(id));
+    }
 }
--- src/main/java/controller/MemberFormController.java
+++ src/main/java/controller/MemberFormController.java
@@ -1,4 +1,13 @@
 package controller;
 
-public class MemberFormController {
+import frontcontroller.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MemberFormController implements Controller {
+
+    @Override
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView("new-form");
+    }
 }
