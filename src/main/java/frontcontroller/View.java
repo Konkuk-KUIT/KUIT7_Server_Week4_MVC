5fd83acb0f3f7a1a870280874b1bb4eee3d5922d
일혁 <dnfjddk2@gmail.com>
2026-04-10 23:08:43 +0900

    Add `render` method to `View` for forwarding requests with model attributes

--- src/main/java/frontcontroller/View.java
+++ src/main/java/frontcontroller/View.java
@@ -1,4 +1,23 @@
 package frontcontroller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Map;
+
 public class View {
+
+    private final String viewName;
+
+    public View(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        model.forEach(request::setAttribute);
+        request.getRequestDispatcher("/WEB-INF/views/" + viewName + ".jsp").forward(request, response);
+    }
 }
